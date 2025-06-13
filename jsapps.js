4835107a39982561fd4fa42074784a285ef8f14e
Santhosh Kumar <santhoshkumarm1508@gmail.com>
2025-06-13 08:08:56 +0530

    first commit

---
+++ jsapps.js
@@ -0,0 +1,9 @@
+function checkEvenOdd(number) {
+  if (number % 2 === 0) {
+    console.log(number + " is even.");
+  } else {
+    console.log(number + " is odd.");
+  }
+}
+checkEvenOdd(7);
+checkEvenOdd(10);
